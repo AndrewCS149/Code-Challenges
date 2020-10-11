38a4f353add246b4e13c6d5e47076d772909ddb4
Andrew Smith <andrewcs149@gmail.com>
2020-10-11 08:42:14 -0700

    Complete plus minus and link to readme.

---
+++ Easy/PlusMinus/PlusMinus/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace PlusMinus
+{
+    internal class Program
+    {
+        // link to challenge - https://www.hackerrank.com/challenges/plus-minus/problem?h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
+        private static void Main(string[] args)
+        {
+            plusMinus(new int[] { 1, 1, 0, -1, -1 });
+        }
+
+        private static void plusMinus(int[] arr)
+        {
+            decimal pos = 0, neg = 0, zero = 0, len = 0;
+            foreach (int num in arr)
+            {
+                if (num < 0) neg++;
+                else if (num > 0) pos++;
+                else zero++;
+
+                len++;
+            }
+            Console.WriteLine(string.Format("{0:N6}", pos / len));
+            Console.WriteLine(string.Format("{0:N6}", neg / len));
+            Console.WriteLine(string.Format("{0:N6}", zero / len));
+        }
+    }
+}
--- README.md
+++ README.md
@@ -11,6 +11,7 @@ A mixture of different hackerrank written in C#.
 - [A Very Big Sum](Easy/VeryBigSum/VeryBigSum)
 - [Simple Array Sum](Easy/SimpleArraySum/SimpleArraySum)
 - [Diagonal Difference](Easy/DiagonalDifference/DiagonalDifference)
+- [Plus Minus](Easy/PlusMinus/PlusMinus)
 
 #### Medium
 
