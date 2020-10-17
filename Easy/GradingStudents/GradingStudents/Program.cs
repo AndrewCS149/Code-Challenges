ebe783f228dbd20ede064af46de8d6c6370a2b96
Andrew Smith <andrewcs149@gmail.com>
2020-10-17 07:51:36 -0700

    Finish GradingStudents.

---
+++ Easy/GradingStudents/GradingStudents/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GradingStudents
+{
+    internal class Program
+    {
+        // easy
+        // link to challenge https://www.hackerrank.com/challenges/grading/problem
+        private static void Main(string[] args)
+        {
+            int gradesCount = Convert.ToInt32(Console.ReadLine().Trim());
+
+            List<int> grades = new List<int>();
+
+            for (int i = 0; i < gradesCount; i++)
+            {
+                int gradesItem = Convert.ToInt32(Console.ReadLine().Trim());
+                grades.Add(gradesItem);
+            }
+
+            List<int> result = gradingStudents(grades);
+            foreach (var item in result)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        public static List<int> gradingStudents(List<int> grades)
+        {
+            for (int i = 0; i < grades.Count; i++)
+            {
+                int diff = 5 - (grades[i] % 5);
+                if (grades[i] + diff < 40)
+                    continue;
+                else if (diff < 3)
+                    grades[i] += diff;
+            }
+
+            return grades;
+        }
+    }
+}
