83ba2c8d46305eb9dda1ce4673eafb84fd16eed5
Andrew Smith <andrewcs149@gmail.com>
2020-10-23 21:33:30 -0700

    Finish day 11.

---
+++ 30DaysOfCode/Easy/Day11-2dArrays/Day11-2dArrays/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Globalization;
+
+namespace Day11_2dArrays
+{
+    internal class Program
+    {
+        // easy
+        // link to challenge - https://www.hackerrank.com/challenges/30-2d-arrays/problem
+        private static void Main(string[] args)
+        {
+            int[][] arr = new int[6][];
+
+            for (int i = 0; i < 6; i++)
+            {
+                arr[i] = Array.ConvertAll(Console.ReadLine().Split(' '), arrTemp => Convert.ToInt32(arrTemp));
+            }
+
+            int sum = 0;
+            for (int i = 0; i < 4; i++)
+            {
+                for (int k = 0; k < 4; k++)
+                {
+                    int tempSum = 0;
+                    tempSum += arr[i][k] + arr[i][k + 1] + arr[i][k + 2];
+                    tempSum += arr[i + 1][k + 1];
+                    tempSum += arr[i + 2][k] + arr[i + 2][k + 1] + arr[i + 2][k + 2];
+                    if (tempSum > sum || i == 0 && k == 0) sum = tempSum;
+                }
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
