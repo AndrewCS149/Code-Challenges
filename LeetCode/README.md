4dc967020dcaa10b0a9b9919bd8a0c4f1937da5e
Andrew Smith <andrewcs149@gmail.com>
2021-01-12 17:23:04 -0800

    Finish Number of Good Pairs

--- LeetCode/README.md
+++ LeetCode/README.md
@@ -3,6 +3,7 @@
 #### Easy
 
 - [Richest Customer Wealth](Easy/RichestCustomerWealth/RichestCustomerWealth)
+- [Number of Good Pairs](Easy/NumberOfGoodPairs/NumberOfGoodPairs)
 
 #### Medium
 
---
+++ LeetCode/easy/NumberOfGoodPairs/NumberOfGoodPairs/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace NumberOfGoodPairs
+{
+    internal class Program
+    {
+        // easy
+        // link to challenge - https://leetcode.com/problems/number-of-good-pairs/
+        private static void Main(string[] args)
+        {
+            Console.WriteLine(NumIdenticalPairs(new int[] { 1, 2, 3, 1, 1, 3 }));
+        }
+
+        public static int NumIdenticalPairs(int[] nums)
+        {
+            // NOT MY SOLUTION
+            int pairs = 0;
+            int[] count = new int[101];
+            foreach (int num in nums)
+                pairs += count[num]++;
+
+            return pairs;
+        }
+    }
+}
