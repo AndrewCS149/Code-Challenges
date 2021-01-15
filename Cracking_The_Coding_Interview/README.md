9cd0d56d133cf363cbdc048809dd6c2b3ca950bf
Andrew Smith <andrewcs149@gmail.com>
2021-01-15 10:42:28 -0800

    Finish URLify

--- Cracking_The_Coding_Interview/README.md
+++ Cracking_The_Coding_Interview/README.md
@@ -2,3 +2,4 @@
 
 - [Is Unique](Is_Unique/Is_Unique)
 - [Check Permutation](CheckPermutation/CheckPermutation)
+- [URLify](URL_ify/URL_ify)
---
+++ Cracking_The_Coding_Interview/URL_ify/URL_ify/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace URL_ify
+{
+    internal class Program
+    {
+        /*
+         URLify
+
+        Write a method to replace all spaces in a string with %20
+         */
+
+        private static void Main(string[] args)
+        {
+            Console.WriteLine(URLify("hello world! "));
+        }
+
+        public static string URLify(string str)
+        {
+            //return str.Replace(" ", "%20");
+
+            // No built in method
+            string newStr = "";
+            foreach (char ch in str)
+            {
+                if (ch == ' ') newStr += "%20";
+                else newStr += ch;
+            }
+
+            return newStr;
+        }
+    }
+}
