bb3cb53fd9412dc4443b9b9283e133b6fceb3c3f
Andrew Smith <andrewcs149@gmail.com>
2021-01-28 10:15:56 -0800

    Finish parking system.

--- LeetCode/README.md
+++ LeetCode/README.md
@@ -4,6 +4,7 @@
 
 - [Richest Customer Wealth](Easy/RichestCustomerWealth/RichestCustomerWealth)
 - [Number of Good Pairs](Easy/NumberOfGoodPairs/NumberOfGoodPairs)
+- [Parking System](Easy/Parking_System/Parking_System)
 
 #### Medium
 
---
+++ LeetCode/easy/Parking_System/Parking_System/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Parking_System
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    public class ParkingSystem
+    {
+        private int[] arr;
+
+        public ParkingSystem(int big, int med, int small)
+        {
+            arr = new int[] { big, med, small };
+        }
+
+        public bool AddCar(int carType)
+        {
+            // ONLINE SOLUTION
+            return arr[carType - 1]-- > 0;
+
+            // MY SOLUTION
+            //if (carType == 1 && arr[0] > 0)
+            //{
+            //    arr[0]--;
+            //    return true;
+            //}
+            //else if (carType == 2 && arr[1] > 0)
+            //{
+            //    arr[1]--;
+            //    return true;
+            //}
+            //else if (carType == 3 && arr[2] > 0)
+            //{
+            //    arr[2]--;
+            //    return true;
+            //}
+
+            //return false;
+        }
+    }
+}
