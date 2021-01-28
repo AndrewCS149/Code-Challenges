72fdc8e373878a3c932c3162fb946a9cb1bfed06
Andrew Smith <andrewcs149@gmail.com>
2021-01-28 10:24:28 -0800

    finish Goal Parser.

--- LeetCode/README.md
+++ LeetCode/README.md
@@ -5,6 +5,7 @@
 - [Richest Customer Wealth](Easy/RichestCustomerWealth/RichestCustomerWealth)
 - [Number of Good Pairs](Easy/NumberOfGoodPairs/NumberOfGoodPairs)
 - [Parking System](Easy/Parking_System/Parking_System)
+- [Goal Parser](Easy/Goal_Parser/Goal_Parser)
 
 #### Medium
 
---
+++ LeetCode/easy/Goal_Parser/Goal_Parser/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Goal_Parser
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public string Interpret(string command)
+        {
+            string newStr = "";
+
+            for (int i = 0; i < command.Length; i++)
+            {
+                if (command[i] == 'G')
+                {
+                    newStr += 'G';
+                }
+                else if (command[i] == '(' && command[i + 1] == ')')
+                {
+                    newStr += 'o';
+                    i++;
+                }
+                else if (command[i] == '(' && command[i + 1] == 'a')
+                {
+                    newStr += "al";
+                    i += 2;
+                }
+            }
+
+            return newStr;
+        }
+    }
+}
