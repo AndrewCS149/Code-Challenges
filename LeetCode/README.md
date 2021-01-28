0568401a02f312af25b2f41bd07aef5b3615e1ee
Andrew Smith <andrewcs149@gmail.com>
2021-01-28 10:42:57 -0800

    Finish Shuffle string.

--- LeetCode/README.md
+++ LeetCode/README.md
@@ -6,6 +6,7 @@
 - [Number of Good Pairs](Easy/NumberOfGoodPairs/NumberOfGoodPairs)
 - [Parking System](Easy/Parking_System/Parking_System)
 - [Goal Parser](Easy/Goal_Parser/Goal_Parser)
+- [Shuffle String](Easy/Shuffle_String/Shuffle_String)
 
 #### Medium
 
---
+++ LeetCode/easy/Shuffle_String/Shuffle_String/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text;
+
+namespace Shuffle_String
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine(RestoreString("codeleet", new int[] { 4, 5, 6, 7, 0, 2, 1, 3 }));
+            Console.WriteLine(RestoreString("abc", new int[] { 0, 1, 2 }));
+            Console.WriteLine(RestoreString("aiohn", new int[] { 3, 1, 4, 2, 0 }));
+        }
+
+        public static string RestoreString(string s, int[] indices)
+        {
+            StringBuilder sb = new StringBuilder(s);
+
+            for (int i = 0; i < indices.Length; i++)
+                sb[indices[i]] = s[i];
+
+            return sb.ToString();
+        }
+    }
+}
