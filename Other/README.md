0a43eec8f0c233c7a0a9f410145f116505ff0988
Andrew Smith <andrewcs149@gmail.com>
2021-02-04 22:16:25 -0800

    Finish challenge

--- Other/README.md
+++ Other/README.md
@@ -3,3 +3,4 @@
 - [IsPalindrome](IsPalindrome/IsPalindrome)
 - [Convert String to Number](StringConvert/StringConvert)
 - [FirstNonRepeatedChar](FirstNonRepeatedChar/FirstNonRepeatedChar)
+- [Convert Number to String](StringConvert2/StringConvert2)
--- Other/StringConvert2/StringConvert2/Program.cs
+++ Other/StringConvert2/StringConvert2/Program.cs
@@ -6,10 +6,8 @@ namespace StringConvert2
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine(ConvertToString(255));
-            Console.WriteLine(ConvertToString(5));
-            Console.WriteLine(ConvertToString(2556));
-            Console.WriteLine(ConvertToString(2558895));
+            Console.WriteLine(StrToNum("55"));
+            Console.WriteLine(StrToNum("10052"));
         }
 
         public static char Units(int num)
@@ -54,6 +52,36 @@ namespace StringConvert2
 
             str += Units(num);
             return str;
+        }
+
+        public static int StrToNum(string str)
+        {
+            int num = 0;
+
+            foreach (char ch in str)
+            {
+                int tmp = ConvertUnit(ch);
+                num = num * 10 + tmp;
+            }
+
+            return num;
+        }
+
+        public static int ConvertUnit(char ch)
+        {
+            switch (ch)
+            {
+                case '1': return 1;
+                case '2': return 2;
+                case '3': return 3;
+                case '4': return 4;
+                case '5': return 5;
+                case '6': return 6;
+                case '7': return 7;
+                case '8': return 8;
+                case '9': return 9;
+                default: return 0;
+            }
         }
     }
 }
