a86b2b22d4c25558b9cb0d5f1b611d4c4fbd764d
Andrew Smith <andrewcs149@gmail.com>
2021-02-04 23:18:43 -0800

    FInish Missing Number

---
+++ Other/Missing_Number/Missing_Number/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Missing_Number
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            int[] arr = new int[99];
+            for (int i = 0, k = 1; i < 99; i++, k++)
+            {
+                if (i == 45) k++;
+                arr[i] = k;
+            }
+
+            int result = MissingNum(arr);
+            Console.WriteLine(result);
+        }
+
+        public static int MissingNum(int[] arr)
+        {
+            int[] tmp = new int[100];
+
+            for (int i = 0; i < arr.Length; i++)
+                tmp[arr[i] - 1] = 1;
+
+            return Array.IndexOf(tmp, 0) + 1;
+        }
+    }
+}
