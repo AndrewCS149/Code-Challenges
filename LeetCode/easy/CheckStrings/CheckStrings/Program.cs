6c9d6f1dd9df6a8060a13a303311bdde1d1c2231
Andrew Smith <andrewcs149@gmail.com>
2021-03-08 13:14:13 -0800

    Finish check strings.

--- LeetCode/easy/CheckStrings/CheckStrings/Program.cs
+++ LeetCode/easy/CheckStrings/CheckStrings/Program.cs
@@ -14,7 +14,7 @@ namespace CheckStrings
 
         public bool ArrayStringsAreEqual(string[] word1, string[] word2)
         {
-            return string.Join(",", word1) == string.Join(",", word2) ? true : false;
+            return string.Join("", word1) == string.Join("", word2) ? true : false;
         }
     }
 }
