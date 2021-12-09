c9503923e1cf0e6c08d183644c369bd13a27c053
Andrew Smith <andrsmith@microsoft.com>
2021-12-09 15:08:17 -0800

    no change

--- Other/FibonacciChecker/FibonacciChecker/Program.cs
+++ Other/FibonacciChecker/FibonacciChecker/Program.cs
@@ -8,7 +8,7 @@
         num1 = num2;// 1
         num2 = fibNumber;
         fibNumber = num1 + num2;
-        if(num == num1 || num == num2 || num == fibNumber)
+        if (num == num1 || num == num2 || num == fibNumber)
         {
             Console.WriteLine("Yes");
             return;
@@ -23,10 +23,10 @@ Solution(54);
 
 static int Fib(int num)
 {
-    if(num == 1 || num == 0)
+    if (num == 1 || num == 0)
     {
         return num;
-    } 
+    }
     else
     {
         return Fib(num - 1) + Fib(num - 2);
@@ -39,12 +39,12 @@ static int FibIterative(int num)
     int fibNumber = 0;
     int num1;
     int num2 = 1;
-    for(int i = 0; i < num; i++)
+    for (int i = 0; i < num; i++)
     {
         num1 = num2;// 1
         num2 = fibNumber;
         fibNumber = num1 + num2;
-        
+
     }
 
     return fibNumber;
