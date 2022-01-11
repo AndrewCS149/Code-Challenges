d89546133a9ebdcb8ae37cae3247b58181559143
Andrew Smith <andrewcs149@gmail.com>
2022-01-11 08:36:34 -0800

    Finish First Palindrome

--- LeetCode/README.md
+++ LeetCode/README.md
@@ -2,6 +2,7 @@
 
 #### Easy
 
+- [Find First Palindrome in Array](Easy/FirstPalindrome/FirstPalindrome)
 - [Sorting the Sentence](Easy/SortingTheSentence/SortingTheSentence)
 - [Max Number of Word Found in Sentences](Easy/MaxWords/MaxWords)
 - [Remove Duplicates from Sorted Array](Easy/RemoveDupesFromArray/RemoveDupesFromArray)
---
+++ LeetCode/easy/FirstPalindrome/FirstPalindrome/Program.cs
@@ -0,0 +1,28 @@
+﻿// https://leetcode.com/problems/find-first-palindromic-string-in-the-array/
+
+string[] test = { "abc", "car", "ada", "racecare", "cool" };
+Console.WriteLine(FirstPalindrome(test));
+
+static string FirstPalindrome(string[] words)
+{
+    foreach (string word in words)
+    {
+        // built in methods
+        //char[] arr = word.ToCharArray();
+        //Array.Reverse(arr);
+
+        //if (new string(arr) == word)
+        //    return word;
+
+        // no built in methods
+        string reverse = "";
+        for (int i = word.Length - 1; i >= 0; i--)
+            reverse += word[i];
+
+        if (reverse == word)
+            return word;
+    }
+
+
+    return "";
+}
