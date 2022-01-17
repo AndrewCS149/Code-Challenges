670537543f787b861107bf5a61c401231a57bc46
Andrew Smith <andrewcs149@gmail.com>
2022-01-17 10:18:06 -0800

    Finish new year chaos.

--- Hackerrank/Medium/NewYearChaos/NewYearChaos/Program.cs
+++ Hackerrank/Medium/NewYearChaos/NewYearChaos/Program.cs
@@ -1,50 +1,39 @@
-﻿using System;
+﻿// https://www.hackerrank.com/challenges/new-year-chaos/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=arrays
 
-namespace NewYearChaos
+using System;
+using System.Collections.Generic;
+
+MinimumBribes(new List<int> { 1, 2, 5, 3, 7, 8, 6, 4 });
+/*
+ 1, 2, 5, 3, 7, 8, 6, 4
+ 1, 2, 3, 5, 7, 8, 6, 4   1
+ 1, 2, 3, 5, 7, 6 8 4    2
+ 1 2 3 5 6 7 8 4       3
+ 1 2 3 5 6 7 4 8        4
+ 1 2 3 5 6 4 7 8        5
+ 1 2 3 5 4 6 7 8         6
+ 1 2 3 4 5 6 7 8        7
+ 
+ 
+ */
+static void MinimumBribes(List<int> q)
 {
-    internal class Program
-    {
-        // link - https://www.hackerrank.com/challenges/new-year-chaos/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=arrays
-        private static void minimumBribes(int[] q)
-        {
-            for (int i = 0; i < q.Length; i++)
-            {
-                if (q[i] - (i + 1) > 2)
-                {
-                    Console.WriteLine("Too chaotic");
-                    return;
-                }
-            }
+    int count = 0;
 
-            int counter = 0;
-            for (int i = 0; i < q.Length - 1; i++)
-            {
-                for (int k = 0; k < q.Length - (i + 1); k++)
-                {
-                    if (q[k] > q[k + 1])
-                    {
-                        int tmp = q[k];
-                        q[k] = q[k + 1];
-                        q[k + 1] = tmp;
-                        counter++;
-                    }
-                }
-            }
-            Console.WriteLine(counter);
+    for (int i = q.Count - 1; i >= 0; i--)
+    {
+        if (q[i] - (i + 1) > 2)
+        {
+            System.Console.WriteLine("Too chaotic");
+            return;
         }
 
-        private static void Main(string[] args)
+        for (int j = Math.Max(0, q[i] - 2); j < i; j++)
         {
-            int t = Convert.ToInt32(Console.ReadLine());
-
-            for (int tItr = 0; tItr < t; tItr++)
-            {
-                int n = Convert.ToInt32(Console.ReadLine());
-
-                int[] q = Array.ConvertAll(Console.ReadLine().Split(' '), qTemp => Convert.ToInt32(qTemp))
-                ;
-                minimumBribes(q);
-            }
+            if (q[j] > q[i])
+                count++;
         }
     }
+
+    Console.WriteLine(count);
 }
