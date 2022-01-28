5661e07ccd5c577546314915af465c46d91e604c
Andrew Smith <andrewcs149@gmail.com>
2022-01-27 20:56:47 -0800

    Impliment O(n^2) solution for LongestPalindrome

--- LeetCode/medium/LongestPalindromicSubstring/LongestPalindromicSubstring/Program.cs
+++ LeetCode/medium/LongestPalindromicSubstring/LongestPalindromicSubstring/Program.cs
@@ -8,36 +8,74 @@ namespace LongestPalindromicSubstring
     {
         private static void Main(string[] args)
         {
-            string tmp = "a";
-            string tmp2 = "baboo";
+            //string tmp = "a";
+            string tmp2 = "babad";
 
-            Console.WriteLine(LongestPalindrome(tmp));
+            //Console.WriteLine(LongestPalindrome(tmp));
             Console.WriteLine(LongestPalindrome(tmp2));
 
         }
 
+        public static string PalindromeCheck(int left, int right, string s, string palindrome)
+        {
+            while (left >= 0 && right < s.Length && s[left] == s[right])
+            {
+                if (right - left + 1 > palindrome.Length)
+                    palindrome = s.Substring(left, right + 1);
+
+                left--;
+                right++;
+            }
+
+            return palindrome;
+        }
+
+
+
         public static string LongestPalindrome(string s)
         {
+            // solution inspired from https://www.youtube.com/watch?v=XYQecbcd6_c
             string palindrome = "";
 
             for (int i = 0; i < s.Length; i++)
             {
-                for (int j = i; j < s.Length; j++)
-                {
-                    string tmp = s.Substring(i, j + 1 - i);
-                    char[] arr = tmp.ToCharArray().Reverse().ToArray();
-                    string tmpReverse = new string(arr);
-
-                    if (tmp == tmpReverse)
-                    {
-                        if (tmp.Length > palindrome.Length)
-                            palindrome = tmp;
-                    }
-
-                }
+                palindrome = PalindromeCheck(i, i, s, palindrome);
+                palindrome = PalindromeCheck(i, i + 1, s, palindrome);
             }
 
             return palindrome;
+
+            // my solution
+            // O(n3)
+            // Slow!
+            //string palindrome = "";
+
+            //for (int i = 0; i < s.Length; i++)
+            //{
+            //    for (int j = i; j < s.Length; j++)
+            //    {
+            //        bool isPalindrome = true;
+            //        string tmp = s.Substring(i, j + 1 - i);
+
+            //        for (int k = 0; k < tmp.Length / 2; k++)
+            //        {
+            //            if (tmp[k] != tmp[tmp.Length - 1 - k])
+            //            {
+            //                isPalindrome = false;
+            //                break;
+            //            }
+            //        }
+
+
+            //        if (isPalindrome)
+            //        {
+            //            if (tmp.Length > palindrome.Length)
+            //                palindrome = tmp;
+            //        }
+            //    }
+            //}
+
+            //return palindrome;
         }
     }
 }
