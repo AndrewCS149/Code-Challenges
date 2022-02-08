9704d3473a6cf218ed5c44a2b584611402ad183e
Andrew Smith <andrewcs149@gmail.com>
2022-02-07 20:01:51 -0800

    Finish 2dTraveler.

--- Other/2dTraveler/2dTraveler/Program.cs
+++ Other/2dTraveler/2dTraveler/Program.cs
@@ -2,14 +2,24 @@
 // The only two directions you can move at a time is down or right.
 
 
-int[][] grid = new int[][]
+Console.WriteLine(Traverse(2, 3));
+Console.WriteLine(Traverse(3, 3));
+Console.WriteLine(Traverse(18, 18));
+
+
+
+static long Traverse(long m, long n, Dictionary<string, long> dict = null)
 {
-    new int[]{1,1,1 },
-    new int[]{2,2,2,2}
-};
+    string key = $"{m},{n}";
 
+    if (dict == null)
+        dict = new Dictionary<string, long>();
 
-static int TraversalWays(int[][] grid)
-{
+    if (dict.ContainsKey(key)) return dict[key];
 
+    if (m == 1 && n == 1) return 1;
+    if (m == 0 || n == 0) return 0;
+
+    dict[key] = Traverse(m - 1, n, dict) + Traverse(m, n - 1, dict);
+    return dict[key];
 }
